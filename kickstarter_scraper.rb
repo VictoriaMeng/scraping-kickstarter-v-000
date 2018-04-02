4462bf2d0085f3978f0bc26cf93e850659524931
Victoria Meng <nights1ststar@protonmail.com>
2018-04-02 00:11:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,11 +1,10 @@
 # require libraries/modules here
 
 require 'pry'
+require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-
-
 
   kickstarter = Nokogiri::HTML(html)
 
